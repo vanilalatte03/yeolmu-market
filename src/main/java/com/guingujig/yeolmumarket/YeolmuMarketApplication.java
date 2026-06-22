9a7b2e5062b9802515aba7325c95b9a94bd1873f
JH <jiho4890@gmail.com>
2026-06-22 12:31:01 +0900

    chore: 초기 프로젝트 커밋

---
+++ src/main/java/com/guingujig/yeolmumarket/YeolmuMarketApplication.java
@@ -0,0 +1,13 @@
+package com.guingujig.yeolmumarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeolmuMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YeolmuMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/guingujig/yeolmumarket/YeolmuMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guingujig.yeolmumarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeolmuMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
