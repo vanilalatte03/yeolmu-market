a40490704612a76294e8d65b4d4398e063ddb7b1
kolyn092 <kolyn092@gmail.com>
2026-06-30 23:06:14 +0900

    refactor: 설정 프로퍼티 바인딩 추가

--- src/main/java/com/guingujig/yeolmumarket/YeolmuMarketApplication.java
+++ src/main/java/com/guingujig/yeolmumarket/YeolmuMarketApplication.java
@@ -1,11 +1,14 @@
 package com.guingujig.yeolmumarket;
 
+import com.guingujig.yeolmumarket.global.config.YeolmuProperties;
 import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
+@EnableConfigurationProperties(YeolmuProperties.class)
 @SpringBootApplication
 public class YeolmuMarketApplication {
 
